56c682fbeff9173a5a9c1c7eee1a09b793f77c47
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-05-26 13:14:46 +0100

    feat(product_ms/settings): adds json render to renderer classes

--- product_ms/settings.py
+++ product_ms/settings.py
@@ -118,6 +118,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
 }
 
 if all(config.CORS_ALLOWED_ORIGINS):
