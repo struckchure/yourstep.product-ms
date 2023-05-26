9648e8f937b298330fb263c9e7f0561a9fca1d85
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-05-26 14:04:16 +0100

    fix(config): adds default secret key to config

--- product_ms/config.py
+++ product_ms/config.py
@@ -4,7 +4,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY", "keep-it-secret")
 DEBUG = int(os.environ.get("DEBUG", 1))
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(" ")
 
